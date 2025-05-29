eaf7a140ab1343e752a3dff33c40e9dc9e165a9d
WSL Misti USIU <misatiquanta@gmail.com>
2025-05-29 16:51:23 +0300

    code: dim_date table population

--- etl_scripts/etl.sql
+++ etl_scripts/etl.sql
@@ -1,3 +1,14 @@
+INSERT INTO dim_date (full_date, day, month, year)
+SELECT
+    DISTINCT transaction_date AS full_date,
+    EXTRACT(DAY FROM transaction_date) AS day,
+    EXTRACT(MONTH FROM transaction_date) AS month,
+    EXTRACT(YEAR FROM transaction_date) AS year
+FROM
+    transactions
+ORDER BY
+    full_date;
+
 
 INSERT INTO fact_sales (product_id, store_id, date_id, quantity_sold, revenue)
 SELECT 
