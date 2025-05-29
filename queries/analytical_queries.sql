49c47fe00c8e99e1f4a8011a7f4d0efcb6d6a227
WSL Misti USIU <misatiquanta@gmail.com>
2025-05-29 16:54:59 +0300

    data: OLAP Queries

--- queries/analytical_queries.sql
+++ queries/analytical_queries.sql
@@ -9,7 +9,7 @@ ORDER BY d.month;
 -- Top Selling Products
 SELECT dp.name, SUM(fs.quantity_sold) AS total_units
 FROM fact_sales fs
-JOIN dim_product dp ON fs.product_id = dp.product_id
+JOIN products dp ON fs.product_id = dp.product_id
 GROUP BY dp.name
 ORDER BY total_units DESC
 LIMIT 5;
